04e03d874b8021b6ccd10232679268d477bdd35d
Zoe <zoeforworks@gmail.com>
2024-12-01 01:12:27 +0800

    fix: 展示資料微調

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ function createSection({ id, title, content }) {
 const projectsData = [
     {
         title: '收納規劃應用',
-        description: '開發一款收納管理應用，運用 SVG 技術實現互動式空間規劃功能，並設計遞歸結構處理收納問題（像俄羅斯娃娃這樣的狀態），利用 Auth0 跟 Google OAuth 2.0 處理會員登入。',
+        description: '開發一款收納管理應用，運用 SVG 技術實現互動式空間規劃功能，並設計遞歸結構處理收納問題（像俄羅斯娃娃這樣的狀態），利用 Auth0 跟 Google OAuth 2.0 處理會員登入，已完成鐵人賽，還未安排上線。',
         tags: ['SVG', 'Auth0', 'Google OAuth 2.0'],
         badgeText: 'IT鐵人賽',
         colorScheme: 'orange',
@@ -326,14 +326,6 @@ const projectsData = [
             {
                 url: 'https://running-man-mu.vercel.app/',
                 label: '成果展示',
-                icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-orange-600" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                        <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                      </svg>`
-            },
-            {
-                url: 'https://www.youtube.com/watch?v=jEQtW26_h0o',
-                label: '教學網址',
                 icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-orange-600" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                         <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
